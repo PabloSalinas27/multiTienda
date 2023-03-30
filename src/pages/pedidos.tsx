e3c26945bf12263fbe58fa2a8c66f529f76adff2
pablo salinas <79252184+PabloSalinas27@users.noreply.github.com>
2023-03-30 19:46:11 +0200

    Accordion en pedidos para mejor gestion

--- src/pages/pedidos.tsx
+++ src/pages/pedidos.tsx
@@ -3,16 +3,24 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Card from "react-bootstrap/Card";
 import { toast } from "react-hot-toast";
-import { usePedidosContext } from "src/contextos/Pedidos";
+import { Pedido, usePedidosContext } from "src/contextos/Pedidos";
 import { useTiendaContext } from "src/contextos/Productos";
+import { ProductoCarrito } from "src/contextos/Carrito";
+import Accordion from "react-bootstrap/Accordion";
 
 export default function Pedidos() {
   const { pedidos, deletePedido } = usePedidosContext();
   const [show, setShow] = useState(false);
   const [id, setId] = useState("");
   const { productos } = useTiendaContext();
+  const calcularTotal = (p: ProductoCarrito[]) =>
+    p.reduce(
+      (acumulado, producto) =>
+        acumulado + producto.cantidad * (productos[producto.id]?.precio || 0),
+      0
+    );
   const handleBorrarPedido = (id: string) => {
-    if (pedidos[id]  === undefined) {
+    if (pedidos[id] === undefined) {
       toast.error("No se ha seleccionado ningun pedido");
       return;
     }
@@ -21,10 +29,15 @@ export default function Pedidos() {
   };
   const pedidoView = Object.entries(pedidos).map(([id, p]) => {
     return (
-      <Card key={p.id}>
-        <Card.Title>
-          <h3>Numero de pedido: {p.id}</h3>
-          <h2>Fecha del pedido: {new Date(p.fecha).toLocaleDateString()}</h2>
+      <Accordion.Item key={p.id} eventKey={p.id.toString()}>
+        <Accordion.Header>
+          <h3>
+            Numero de pedido: {p.id}. Fecha del pedido:{" "}
+            {new Date(p.fecha).toLocaleDateString()}. Total:{" "}
+            {calcularTotal(p.productos)}€
+          </h3>
+        </Accordion.Header>
+        <Accordion.Body>
           <div>
             <i>Repartir a: </i>
             <i>
@@ -38,26 +51,28 @@ export default function Pedidos() {
             <i>{p.datosReparto.apellidos},</i>
             <i>{" " + p.datosReparto.nombre + ")"}</i>
           </div>
-        </Card.Title>
-        {p.productos.map((prod) => {
-          return (
-            <div key={p.id.toString() + prod.id.toString()}>
-              <h4>{productos[prod.id]?.nombre || "Producto no encontrado"}</h4>
-              <h4>{prod.cantidad}</h4>
-            </div>
-          );
-        })}
-        <Button
-          variant="danger"
-          style={{ maxWidth: "20em" }}
-          onClick={() => {
-            setId(id);
-            setShow(true);
-          }}
-        >
-          Borrar Pedido
-        </Button>
-      </Card>
+          {p.productos.map((prod) => {
+            return (
+              <div key={p.id.toString() + prod.id.toString()}>
+                <h4>
+                  {productos[prod.id]?.nombre || "Producto no encontrado"}
+                </h4>
+                <h4>{prod.cantidad}</h4>
+              </div>
+            );
+          })}
+          <Button
+            variant="danger"
+            style={{ maxWidth: "20em" }}
+            onClick={() => {
+              setId(id);
+              setShow(true);
+            }}
+          >
+            Borrar Pedido
+          </Button>
+        </Accordion.Body>
+      </Accordion.Item>
     );
   });
   return (
@@ -77,7 +92,7 @@ export default function Pedidos() {
         </Modal.Footer>
       </Modal>
       <h1>Pedidos</h1>
-      {pedidoView}
+      <Accordion>{pedidoView}</Accordion>
     </>
   );
 }
